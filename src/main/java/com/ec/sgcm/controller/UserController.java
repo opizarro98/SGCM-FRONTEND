86c5f0c437f573cfcbdcf3c761a4f478b4c5e9c1
opizarro98 <opizarrog9808@gmail.com>
2024-10-09 22:42:02 -0500

    OP -  correcion de login para conectar al front

--- src/main/java/com/ec/sgcm/controller/UserController.java
+++ src/main/java/com/ec/sgcm/controller/UserController.java
@@ -2,11 +2,13 @@ package com.ec.sgcm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ec.sgcm.model.User;
@@ -14,6 +16,7 @@ import com.ec.sgcm.model.dto.loginCredentialsDTO;
 import com.ec.sgcm.services.UserService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/userRest")
 public class UserController {
 
@@ -32,9 +35,11 @@ public class UserController {
     // }
 
     @GetMapping("/loginToSystem")
-    public ResponseEntity<User> loginToSystem(@RequestBody loginCredentialsDTO loginCredentialsDTO) {
+    public ResponseEntity<User> loginToSystem(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password) {
         return ResponseEntity.ok(
-                userService.findUserByUsername(loginCredentialsDTO.getUsername(), loginCredentialsDTO.getPassword()));
+                userService.findUserByUsername(username, password));
     }
 
 }
