af50edba24a92c6b10f4a539fcf3dff4242e6683
opizarro98 <opizarrog9808@gmail.com>
2024-10-19 12:30:23 -0500

    OP - cors acces all cambiado

--- src/main/java/com/ec/sgcm/auth/AuthController.java
+++ src/main/java/com/ec/sgcm/auth/AuthController.java
@@ -12,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = { "http://localhost:4200" })
+@CrossOrigin(origins = { "*" })
 public class AuthController {
 
     private final AuthService authService;
